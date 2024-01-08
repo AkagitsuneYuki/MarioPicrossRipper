f46bb2e7fe1832ed54f5004cfae2476d7e18b98b
AkagitsuneYuki <michael.lovallo@gmail.com>
2024-01-08 11:23:07 -0500

    Ready for release
    -Bump version from 1.0 to 1.1
    -Added icon the the main .exe
    Let's goooooooooooo!!!!

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MarioPicrossRipper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A small utility for viewing and extracting the solutions to Mario's Picross.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MarioPicrossRipper")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
